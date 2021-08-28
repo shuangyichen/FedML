6a1404f067efa6aef3727b668f3deab5fc2b9537
shuangyichen <meetshuangyichen@outlook.com>
2021-08-27 20:28:54 -0400

    environment config

--- CI-install.sh
+++ CI-install.sh
@@ -21,7 +21,8 @@ echo "conda activate fedml"
 conda activate fedml
 
 # Install PyTorch (please visit pytorch.org to check your version according to your physical machines
-conda install pytorch torchvision cudatoolkit=10.2 -c pytorch
+#conda install pytorch torchvision cudatoolkit=10.2 -c pytorch
+conda install pytorch torchvision torchaudio cpuonly -c pytorch
 
 # Install MPI
 conda install -c anaconda mpi4py
@@ -44,25 +45,25 @@ sh download_and_unzip.sh
 cd ../../
 
 # 2. FederatedEMNIST
-cd ./data/FederatedEMNIST
-sh download_federatedEMNIST.sh
-cd ../../
+#cd ./data/FederatedEMNIST
+#sh download_federatedEMNIST.sh
+#cd ../../
 
 # 3. shakespeare
-cd ./data/shakespeare
-sh download_shakespeare.sh
-cd ../../
+#cd ./data/shakespeare
+#sh download_shakespeare.sh
+#cd ../../
 
 
 # 4. fed_shakespeare
-cd ./data/fed_shakespeare
-sh download_shakespeare.sh
-cd ../../
+#cd ./data/fed_shakespeare
+#sh download_shakespeare.sh
+#cd ../../
 
 # 5. fed_cifar100
-cd ./data/fed_cifar100
-sh download_fedcifar100.sh
-cd ../../
+#cd ./data/fed_cifar100
+#sh download_fedcifar100.sh
+#cd ../../
 
 # 6. stackoverflow
 # cd ./data/stackoverflow
@@ -70,16 +71,16 @@ cd ../../
 # cd ../../
 
 # 7. CIFAR10
-cd ./data/cifar10
-sh download_cifar10.sh
-cd ../../
+#cd ./data/cifar10
+#sh download_cifar10.sh
+#cd ../../
 
 # 8. CIFAR100
-cd ./data/cifar100
-sh download_cifar100.sh
-cd ../../
+#cd ./data/cifar100
+#sh download_cifar100.sh
+#cd ../../
 
 # 9. CINIC10
-cd ./data/cinic10
-sh download_cinic10.sh > cinic10_downloading_log.txt
-cd ../../
+#cd ./data/cinic10
+#sh download_cinic10.sh > cinic10_downloading_log.txt
+#cd ../../
